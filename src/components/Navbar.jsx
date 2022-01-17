0d0a9d1d31caff4e86f160ee6aaf8204b30cdaba
hiba <hiba.mashfij@gmail.com>
2022-01-17 22:17:52 +0300

    yarn fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,11 +89,25 @@ const Navbar = () => {
                     {item.name}
                   </a>
                 ))}
+                <div className="ml-2 mt-2">
+                  {colorTheme === 'dark' ? (
+                    <button onClick={() => editTheme(colorTheme)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-700 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                      </svg>
+                    </button>
+                  ) : (
+                    <button onClick={() => editTheme(colorTheme)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-700 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                      </svg>
+                    </button>
+                  )}
+                </div>
               </div>
               {/* <NavLink to="#" className="block w-full px-5 py-3 text-center font-medium text-white dark:bg-green hover:bg-gray-100">
                 Log in
               </NavLink> */}
-              <button onClick={() => editTheme(colorTheme)}>Toggle dark</button>
             </div>
           </Popover.Panel>
         </Transition>
